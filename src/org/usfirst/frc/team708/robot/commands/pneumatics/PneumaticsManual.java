8bc07db919407462cf21a768a2c5a4e92d86c751
James Makovics <jmakovics1@gmail.com>
2018-01-15 15:30:23 -0500

    Commented out manual pneumatics

--- src/org/usfirst/frc/team708/robot/commands/pneumatics/PneumaticsManual.java
+++ src/org/usfirst/frc/team708/robot/commands/pneumatics/PneumaticsManual.java
@@ -1,45 +1,45 @@
-package org.usfirst.frc.team708.robot.commands.pneumatics;
-
-import java.awt.Robot;
-
-import org.usfirst.frc.team708.robot.Constants;
-
-import edu.wpi.first.wpilibj.command.Command;
-
-public class PneumaticsManual extends Command {
-
-
-    public PneumaticsManual() {
-//    	requires(Robot.feeder);
-//    	requires(Robot.intake_ball);
-//    	requires(Robot.drivetrain);
-//    	requires(Robot.shooter);
-    }
-    
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	Robot.PneumaticsTest.set(Constants.INTAKE_FORWARD);
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {    	
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	return(false);
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.feeder.stop();
-    	Robot.intake_ball.stop();
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems are scheduled to run
-    protected void interrupted() {
-    	end();
-    }
-}
+//package org.usfirst.frc.team708.robot.commands.pneumatics;
+//
+//import java.awt.Robot;
+//
+//import org.usfirst.frc.team708.robot.Constants;
+//
+//import edu.wpi.first.wpilibj.command.Command;
+//
+//public class PneumaticsManual extends Command {
+//
+//
+//    public PneumaticsManual() {
+////    	requires(Robot.feeder);
+////    	requires(Robot.intake_ball);
+////    	requires(Robot.drivetrain);
+////    	requires(Robot.shooter);
+//    }
+//    
+//
+//    // Called just before this Command runs the first time
+//    protected void initialize() {
+//    	Robot.PneumaticsTest.set();
+//    }
+//
+//    // Called repeatedly when this Command is scheduled to run
+//    protected void execute() {    	
+//    }
+//
+//    // Make this return true when this Command no longer needs to run execute()
+//    protected boolean isFinished() {
+//    	return(false);
+//    }
+//
+//    // Called once after isFinished returns true
+//    protected void end() {
+//    	Robot.feeder.stop();
+//    	Robot.intake_ball.stop();
+//    }
+//
+//    // Called when another command which requires one or more of the same
+//    // subsystems are scheduled to run
+//    protected void interrupted() {
+//    	end();
+//    }
+//}
