fb756a2b673551ff5e154f39ae9f8722fd062616
James Makovics <jmakovics1@gmail.com>
2018-01-14 20:03:37 -0500

    Fixed formatting for SWOLENOIDS

--- src/org/usfirst/frc/team708/robot/RobotMap.java
+++ src/org/usfirst/frc/team708/robot/RobotMap.java
@@ -70,13 +70,13 @@ public class RobotMap {
 	public static final int armPotentiometer	= 0;
 	
 	// PCM Ports
-//	public static final int 				= 0;
-//	public static final int   				= 1;
-//	public static final int  				= 2;
-//	public static final int 				= 3;
-//	public static final int 				= 4;
-//	public static final int 				= 5;
-//	public static final int					= 6;
-//	public static final int 				= 7;
+	public static final int ShiftingDrive  = 0; //Shifts Drivetrain from High gear to low gear
+    public static final int SwitchWheels   = 1; //Changes from omni to coleson
+	public static final int Grabber 	   = 2; //Changes grabber state for 
+//	public static final int 			   = 3;
+//	public static final int 			   = 4;
+//	public static final int 			   = 5;
+//	public static final int				   = 6;
+//	public static final int 			   = 7;
 	
 }
